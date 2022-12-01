9e42a424a240c4dfde58fb7b34fa065c97a7c094
DailyPS <55373035+DailyPS@users.noreply.github.com>
2022-12-02 02:07:47 +0900

    Crawl LoL World Championship Octobor json

--- public/crawl.js
+++ public/crawl.js
@@ -1,6 +1,7 @@
+const fs = require("fs")
 const https = require("https")
 const html_parser = require("node-html-parser")
-const url = "https://game.naver.com/esports/League_of_Legends/schedule/world_championship?date=2022-11"
+const url = "https://game.naver.com/esports/League_of_Legends/schedule/world_championship?date=2022-10"
 
 const header = {
     headers: {
@@ -26,15 +27,37 @@ https.get(
             schedule_json = JSON.parse(schedule_json)
             monthly_schedule_list = schedule_json.props.initialState.schedule.monthSchedule
 
+            var finalData = new Object()
+            var monthList = new Array()
+
             for (i = 0; i < monthly_schedule_list.length; i++) {
                 let daily_schedule = monthly_schedule_list[i].schedules
+                var dailyList = new Array()
 
                 for (j = 0; j < daily_schedule.length; j++) {
+                    var daily_data = new Object()
                     let detail = daily_schedule[j]
-                    let track = detail.date + " " + detail.time + " " + detail.homeTeam.name + " " + detail.awayTeam.name
-                    console.log(track)
+
+                    daily_data.date = detail.date
+                    daily_data.time = detail.time
+                    daily_data.homeTeam = detail.homeTeam.name
+                    daily_data.awayTeam = detail.awayTeam.name
+
+                    dailyList.push(daily_data)
                 }
+
+                var monthly_data = new Object()
+
+                monthly_data.dayGame = dailyList
+
+                monthList.push(monthly_data)
             }
+
+            finalData.monthGame = monthList
+
+            var jsonData = JSON.stringify(finalData)
+
+            fs.writeFileSync("lol.json", jsonData)
         })
     }
 )
