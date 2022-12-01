8e65b670f6b71a2ffd20f2a78056ce8bd12d18d5
DailyPS <55373035+DailyPS@users.noreply.github.com>
2022-12-02 00:37:47 +0900

    Extract date, time, team name from html.

--- public/crawl.js
+++ public/crawl.js
@@ -1,6 +1,6 @@
 const https = require("https")
-const parser = require("node-html-parser")
-const url = "https://game.naver.com/esports/general/schedule/vct?date=2022-09"
+const html_parser = require("node-html-parser")
+const url = "https://game.naver.com/esports/League_of_Legends/schedule/world_championship?date=2022-11"
 
 const header = {
     headers: {
@@ -11,18 +11,26 @@ const header = {
 https.get(
     url, header, (res) => {
         let data = "";
+
         res.on("data", (chunk) => {
             data += chunk;
         });
+
         res.on("end", () => {
-            const root = parser.parse(data)
+            const root = html_parser.parse(data)
             const list = root.querySelectorAll("#__NEXT_DATA__")
             list.forEach((item) => {
                 schedule_json = item.innerText.trim()
             })
 
             schedule_json = JSON.parse(schedule_json)
-            console.log(schedule_json.props.initialState.schedule.monthSchedule[0].schedules)
+            schedule_list = schedule_json.props.initialState.schedule.monthSchedule
+
+            for (i = 0; i < schedule_list.length; i++) {
+                let detail = schedule_list[i].schedules[0]
+                let track = detail.date + " " + detail.time + " " + detail.homeTeam.name + " " + detail.awayTeam.name
+                console.log(track)
+            }
         })
     }
 )
